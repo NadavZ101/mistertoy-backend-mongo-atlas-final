9dc71bb1e41907412796cb99d2a3484b3bf070d9
NadavZ101 <nadavzivyon@gmail.com>
2024-04-02 08:04:51 +0300

    New build from front

--- config/dev.js
+++ config/dev.js
@@ -2,3 +2,10 @@ export default {
   dbURL: 'mongodb+srv://nadavzivyon:nz1984@cluster0.0t7ctqp.mongodb.net/',
   dbName: 'toy_db',
 }
+
+// const dbConfig = {
+//   dbURL: process.env.MONGO_URL || 'mongodb+srv://nadavzivyon:nz1984@cluster0.0t7ctqp.mongodb.net/',
+//   dbName: process.env.DB_NAME || 'toy_db',
+// };
+
+// export default dbConfig;
